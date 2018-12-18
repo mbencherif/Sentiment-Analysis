1029d8709584a7938bf03131533f173a9e8e4019
Caiwei Wu <aliintern18@gmail.com>
2018-12-18 15:47:27 -0500

    Adding stemmer and lemmatizer
    Accuracy slightly goes up.

--- random_forest.py
+++ random_forest.py
@@ -3,16 +3,24 @@
 import pandas as pd
 from bs4 import BeautifulSoup  
 import re
+
+from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk.stem.porter import *
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 def pre_process(text):
-    text=BeautifulSoup(text).get_text()
-    text=re.sub("[^a-zA-Z]"," ", text.lower())  
+    text=BeautifulSoup(text, features="html.parser").get_text()
+    text=re.sub("[^a-zA-Z]"," ", text.lower())
+    stemmer = PorterStemmer()
+    stemmed_words = list(map(stemmer.stem, text.split()))
+    lemmer = WordNetLemmatizer()
+    stemmed_words = [lemmer.lemmatize(w) for w in stemmed_words]
     text=[w for w in text.split() if not w in stop_w]
+    text = [w for w in stemmed_words if not w in stop_w]
     text = " ".join(text)
     return text
 
@@ -28,7 +36,14 @@ if __name__ == '__main__':
     vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None,max_features = 5000)
     train_data_features = vectorizer.fit_transform(list(train.clean_review)).toarray()
     test_data_features = vectorizer.transform(list(test.clean_review)).toarray()
-    
+    # vocab = vectorizer.get_feature_names()
+    # print(vocab)
+    # # Sum up the counts of each vocabulary word
+    # dist = np.sum(train_data_features, axis=0)
+    # # For each, print the vocabulary word and the number of times it
+    # # appears in the training set
+    # for tag, count in sorted(zip(vocab, dist), key=lambda x: x[1], reverse=True):
+    #     print(count, tag)
     forest = RandomForestClassifier(n_estimators = 100) 
     forest = forest.fit( train_data_features, train["sentiment"] )
     values=forest.predict(test_data_features)
@@ -46,7 +61,7 @@ if __name__ == '__main__':
             pass
     accuracy=correct/total
 
-    print("Using, random forest, the accuracy is {}".format(accuracy))
+    print("Using random forest, the accuracy is {}".format(accuracy))
 
 
 
